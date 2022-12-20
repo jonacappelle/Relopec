f5ed2163b3661c6805d48fc87be4b45722ee99e5
jonacappelle <jonacappelle@hotmail.com>
2022-12-20 14:57:34 +0100

    Change run script to run continuously

--- runRelopec.sh
+++ runRelopec.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+@echo off 
+echo "RELOPEC Test Started"
 
-sudo ./libiec61850/relopec/sv_subscriber/sv_subscriber | python RELOPEC_algorithm.py
+while :
+do
+    sudo ./libiec61850/relopec/sv_subscriber/sv_subscriber | python RELOPEC_algorithm.py || break;
+done
+
 
